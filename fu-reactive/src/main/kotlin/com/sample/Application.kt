0832e59fc6a91c372c5159a942bf97edfcaf65d9
Josh Long <josh@joshlong.com>
2018-11-27 02:23:26 +0100

    fu

--- fu-reactive/src/main/kotlin/com/sample/Application.kt
+++ fu-reactive/src/main/kotlin/com/sample/Application.kt
@@ -1,53 +1,12 @@
-/*
- * Copyright 2002-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.sample
 
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.fu.kofu.application
-import org.springframework.fu.kofu.configuration
-import org.springframework.fu.kofu.mongo.embedded
-import org.springframework.fu.kofu.mongo.mongodb
-import org.springframework.fu.kofu.web.jackson
-import org.springframework.fu.kofu.web.mustache
-import org.springframework.fu.kofu.web.server
 
 val app = application {
 	properties<SampleProperties>("sample")
- 	beans {
-		bean<UserHandler>()
-		bean<UserRepository>()
-	}
-	listener<ApplicationReadyEvent> {
-		ref<UserRepository>().init()
-	}
-	mongodb {
-		embedded()
-	}
-	server {
-		port = if (profiles.contains("test")) 8181 else 8080
-		mustache()
-		codecs {
-			string()
-			jackson()
-		}
-		router {
-
-		}
-	}
+	import(dataConfig)
+	import(webConfig)
 }
 
 fun main() {
