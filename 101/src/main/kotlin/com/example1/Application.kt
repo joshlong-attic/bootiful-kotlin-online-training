d03be553f58356df621e517decaaba3a6a21f6d8
Josh Long <josh@joshlong.com>
2018-12-18 10:56:53 -0800

    kotlin coroutines.

--- 101/src/main/kotlin/com/example1/Application.kt
+++ 101/src/main/kotlin/com/example1/Application.kt
@@ -1,6 +1,7 @@
 package com.example1
 
 import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicLong
 
 
 // Uses Kotlin 1.3.0 and
@@ -11,15 +12,29 @@ import kotlinx.coroutines.*
 //  <version>1.0.1</version>
 // </dependency>
 
+val globalId = AtomicLong()
+
 data class Customer(val id: Int = 0, val name: String)
 
 fun main(args: Array<String>) {
 
+	//  extension methods
+	fun Customer.isValid(): Boolean = this.name.isNotEmpty()
+
+	// overloaded methods
+	val names = mutableListOf<String>()
+	names += "Bob"
+	println("the size of the names array is ${names.size} ")
+
 	// functions as a first class citizen
 	val myFunc: (String) -> Customer = { Customer(name = it) }
+
 	arrayOf("Josh", "Madhura", "Olga", "Zhen", "Kimly", "Tammie")
 			.map(myFunc)
-			.forEach { println(it) }
+			.forEach {
+				println(it)
+				println(it.isValid())
+			}
 
 	// coroutines
 	val deferred = (1..1_000_000).map { n ->
