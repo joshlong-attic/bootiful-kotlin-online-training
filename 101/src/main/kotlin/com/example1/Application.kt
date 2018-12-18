765b1ed208cb1c498ada72f74d5e054e71f56b1f
Josh Long <josh@joshlong.com>
2018-12-18 10:45:55 -0800

    kotlin coroutines.

--- 101/src/main/kotlin/com/example1/Application.kt
+++ 101/src/main/kotlin/com/example1/Application.kt
@@ -1,11 +1,35 @@
 package com.example1
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
-// Bruce Eckel has a great Kotlin book
+
+// Uses Kotlin 1.3.0 and
+//
+// <dependency>
+//  <groupId>org.jetbrains.kotlinx</groupId>
+//  <artifactId>kotlinx-coroutines-core</artifactId>
+//  <version>1.0.1</version>
+// </dependency>
 
 data class Customer(val id: Int = 0, val name: String)
 
 fun main(args: Array<String>) {
+
+	val deferred = (1..1_000_000)
+			.map { n ->
+				GlobalScope.async {
+					delay(1000)
+					n
+				}
+			}
+
+	runBlocking {
+		val sum = deferred.sumBy { it.await() }
+		println("the sum is $sum")
+	}
 
 	val myFunc: (String) -> Customer = { Customer(name = it) }
 
